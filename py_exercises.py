b67551b280db675b849aad6c38758ba232469d20
rabahZebiri <rabahzebiri01@gmail.com>
2025-03-20 15:41:19 +0100

    Calculating the surface area of a room

--- py_exercises.py
+++ py_exercises.py
@@ -1,10 +1,14 @@
-available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
-requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
-
-for requested_topping in requested_toppings:
-    if requested_topping in available_toppings:
-        print(f"Adding {requested_topping}.")
-    else:
-        print(f"Sorry, we don't have {requested_topping}.")
-
-print("\nFinished making your pizza!")
+room_length=float(input("Please enter the room's length in feets:\n"))
+room_width=float(input("Please enter the room's width in feets:\n"))
+if room_width<=0  and room_width <=0 :
+    print("please enter a correct values")
+#VARIABLES AND CONSTANTS
+CONVERSION_FACTOR=0.092903
+square_feet_area=room_length*room_width
+square_meters_area=square_feet_area*CONVERSION_FACTOR
+#END VARIABLES AND CONSTANTS
+#AVOIDING THE FLOATING NUMBER
+square_meters_area=round(square_meters_area,2)
+square_feet_area=round(square_feet_area,2)
+print(f"The surface area of the room in square feet is : {square_feet_area} ")
+print(f"The surface area of the room in square meters is : {square_meters_area} ")
