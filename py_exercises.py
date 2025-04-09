b184d7f39854d751887af5c49f04cc7ea8c81abf
rabahZebiri <rabahzebiri01@gmail.com>
2025-04-09 16:20:59 +0100

    Calculating the surface area of a room

--- py_exercises.py
+++ py_exercises.py
@@ -1,14 +1,30 @@
-room_length=float(input("Please enter the room's length in feets:\n"))
-room_width=float(input("Please enter the room's width in feets:\n"))
-if room_width<=0  and room_width <=0 :
-    print("please enter a correct values")
-#VARIABLES AND CONSTANTS
-CONVERSION_FACTOR=0.092903
-square_feet_area=room_length*room_width
-square_meters_area=square_feet_area*CONVERSION_FACTOR
-#END VARIABLES AND CONSTANTS
-#AVOIDING THE FLOATING NUMBER
-square_meters_area=round(square_meters_area,2)
-square_feet_area=round(square_feet_area,2)
-print(f"The surface area of the room in square feet is : {square_feet_area} ")
-print(f"The surface area of the room in square meters is : {square_meters_area} ")
+while True:
+    room_length = input("Please enter the room's length in feet:\n")
+    room_width = input("Please enter the room's width in feet:\n")
+
+    try:
+        room_length = float(room_length)
+        room_width = float(room_width)
+    except ValueError:
+        print("You should enter a valid number.")
+        continue  # Loop again if there's a ValueError
+
+    if room_length <= 0 or room_width <= 0:
+        print("Please enter valid positive values for both dimensions.")
+        continue  # Loop again if either dimension is non-positive
+
+    # If input is valid, break the loop
+    break
+
+# VARIABLES AND CONSTANTS
+CONVERSION_FACTOR = 0.092903
+square_feet_area = room_length * room_width
+square_meters_area = square_feet_area * CONVERSION_FACTOR
+# END VARIABLES AND CONSTANTS
+
+# AVOIDING THE FLOATING NUMBER
+square_meters_area = round(square_meters_area, 2)
+square_feet_area = round(square_feet_area, 2)
+
+print(f"The surface area of the room in square feet is: {square_feet_area}")
+print(f"The surface area of the room in square meters is: {square_meters_area}")
